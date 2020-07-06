3900af1e9cec13927a46bebcc505a53b72e97b51
jerev7 <verdet.jeremie@gmail.com>
2020-07-06 14:27:08 +0200

    full code works

--- Book.js
+++ Book.js
@@ -10,13 +10,14 @@ export class Book {
 	readBook(page) {
 		if (page < 1 || page > this.pages) {
 			return 0
-		} else if (page >= 1 && page < this.pages) {
+		} if (page >= 1 && page < this.pages) {
 			this.currentPage = page;
 			return 1;
-		} else if (page === this.pages) {
+		} if (page === this.pages) {
 			this.read = true;
 			return 1;
 		}
+	}
 
 }
 
@@ -25,3 +26,5 @@ export const books = [];
 let book1 = new Book("Le mystère Napoléon", "Steve Berry", "livre plutôt cool", 530, 250);
 let book2 = new Book("Mort sur le Nil", "Agatha Christie", "suspense !", 420, 3);
 let book3 = new Book("Les rivières pourpres", "Jean-Christophe Grangé", "thriller", 600, 600);
+
+books.push(book1, book2, book3);
